0c030e7452a6fd8cfced45bda167372c74233bf4
Elio <elion628@gmail.com>
2024-08-29 11:03:21 +0300

    Initial Creation

---
+++ DummyApp/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DummyApp.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TestController:ControllerBase
+{
+    [HttpGet("GetUser")]
+    public IActionResult GetUser()
+    {
+        return Ok("elio");
+    }
+}
